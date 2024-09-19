39e2a62eb7e5e926db5ed53ec668caa3119b3e9f
Noah Erasmus <noah@bigbrave.co.za>
2024-09-19 10:13:38 +0200

    Final tweaks

--- components/UserDetails.tsx
+++ components/UserDetails.tsx
@@ -8,11 +8,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const UserDetails = ({ name, email, phone, website, company, id }: User) => {
   const [detailsRevealed, setDetailsRevealed] = useState(false);
   const [showInput, setShowInput] = useState(false);
-  // Prepare correct answer state variable
   const [correctAnswer, setCorrectAnswer] = useState<number | null>(null);
   const [userInput, setUserInput] = useState<number | undefined>(undefined);
   const [detailsLocked, setDetailsLocked] = useState(false);
-  const [attempts, setAttempts] = useState(0);
   const [num1, setNum1] = useState<number | null>(null);
   const [num2, setNum2] = useState<number | null>(null);
 
@@ -21,22 +19,12 @@ const UserDetails = ({ name, email, phone, website, company, id }: User) => {
     setNum1(Math.floor(Math.random() * 90 + 10));
     setNum2(Math.floor(Math.random() * 90 + 10));
 
-    // Get failed attempt history from local storage
-    const failedAttemptsData = localStorage.getItem("failedAttempts");
-    if (failedAttemptsData) {
-      const failedAttempts: { userId: number; attempts: number }[] =
-        JSON.parse(failedAttemptsData);
-      const relevantUser = failedAttempts.find(
-        (attempt) => attempt.userId === id
-      );
-
-      setAttempts(relevantUser?.attempts || 0);
-
-      if (relevantUser && relevantUser.attempts >= 2) {
-        setDetailsLocked(true);
-      }
+    // Get total failed attempts from local storage
+    const totalFailedAttempts = localStorage.getItem("totalFailedAttempts");
+    if (totalFailedAttempts && parseInt(totalFailedAttempts) >= 2) {
+      setDetailsLocked(true);
     }
-  }, [id]);
+  }, []);
 
   const handleReveal = () => {
     if (num1 === null || num2 === null) return;
@@ -47,36 +35,24 @@ const UserDetails = ({ name, email, phone, website, company, id }: User) => {
   };
 
   const handleSubmit = () => {
-    console.log(userInput);
-    console.log(correctAnswer);
     if (userInput === correctAnswer) {
       // Reveal details if the answer is correct
       setDetailsRevealed(true);
       setShowInput(false);
     } else {
-      // Increment attempts and store in local storage if incorrect
-      setAttempts(attempts + 1);
-      let failedAttemptsData = localStorage.getItem("failedAttempts");
-      let failedAttempts = failedAttemptsData
-        ? JSON.parse(failedAttemptsData)
-        : [];
+      // Increment total failed attempts and store in local storage if incorrect
+      let totalFailedAttempts = localStorage.getItem("totalFailedAttempts");
+      totalFailedAttempts = totalFailedAttempts
+        ? (parseInt(totalFailedAttempts) + 1).toString()
+        : "1";
 
-      const relevantUser = failedAttempts.findIndex(
-        (attempt: { userId: number; attempts: number }) => attempt.userId === id
-      );
+      localStorage.setItem("totalFailedAttempts", totalFailedAttempts);
 
-      if (relevantUser !== -1) {
-        failedAttempts[relevantUser].attempts += 1;
-      } else {
-        failedAttempts.push({ userId: id, attempts: 1 });
-      }
-
-      localStorage.setItem("failedAttempts", JSON.stringify(failedAttempts));
-
-      // Lock details if there are 2 failed attempts
-      if (failedAttempts[relevantUser]?.attempts >= 2) {
+      // Lock details if there are 2 or more failed attempts
+      if (parseInt(totalFailedAttempts) >= 2) {
         setDetailsLocked(true);
       } else {
+        // Generate new numbers if they haven't reached the limit yet
         const newNum1 = Math.floor(Math.random() * 90 + 10);
         const newNum2 = Math.floor(Math.random() * 90 + 10);
         setNum1(newNum1);
@@ -92,7 +68,7 @@ const UserDetails = ({ name, email, phone, website, company, id }: User) => {
 
   return (
     <div className="rounded-md bg-white shadow-lg p-6 w-full grid grid-cols-1 md:grid-cols-12 divide-y-[1px] divide-black">
-      <div className="md:col-span-12 flex flex-col md:flex-row items-center justify-between mb-4">
+      <div className="md:col-span-12 flex flex-col md:flex-row items-start lg:items-center justify-between mb-4 w-full">
         <div className="flex flex-col gap-2">
           <h2 className="text-black text-4xl font-bold">{name}</h2>
 
@@ -100,34 +76,53 @@ const UserDetails = ({ name, email, phone, website, company, id }: User) => {
           <div className="flex flex-col md:flex-row items-start md:items-center gap-4">
             <div className="flex items-center gap-2">
               <FontAwesomeIcon icon={faEnvelope} className="h-3 text-black" />
-              <a
-                href={`mailto:${email}`}
-                className={`text-black ${
-                  !detailsRevealed
-                    ? "blur-sm unselectable pointer-events-none"
-                    : "blur-none"
-                } transition-all duration-300`}
-              >
-                {email}
-              </a>
+              {!detailsRevealed ? (
+                <p className={`text-black transition-all duration-300`}>
+                  {email.split("@")[0]}
+                  <span className="blur-sm unselectable pointer-events-none">
+                    fakemail.com
+                  </span>
+                </p>
+              ) : (
+                <a
+                  href={`mailto:${email}`}
+                  className={`text-black ${
+                    !detailsRevealed
+                      ? "blur-sm unselectable pointer-events-none"
+                      : "blur-none"
+                  } transition-all duration-300`}
+                >
+                  {email}
+                </a>
+              )}
             </div>
 
             <div className="flex items-center gap-2">
               <FontAwesomeIcon icon={faPhone} className="h-3 text-black" />
-              <a
-                href={`tel:${phone}`}
-                className={`text-black ${
-                  !detailsRevealed
-                    ? "blur-sm unselectable pointer-events-none"
-                    : "blur-none"
-                } transition-all duration-300`}
-              >
-                {phone}
-              </a>
+              {!detailsRevealed ? (
+                <p className={`text-black transition-all duration-300`}>
+                  {phone.split("-")[0]}-{phone.split("-")[1]}
+                  <span className="blur-sm unselectable pointer-events-none">
+                    -fake-num
+                  </span>
+                </p>
+              ) : (
+                <a
+                  href={`tel:${phone}`}
+                  className={`text-black ${
+                    !detailsRevealed
+                      ? "blur-sm unselectable pointer-events-none"
+                      : "blur-none"
+                  } transition-all duration-300`}
+                >
+                  {phone}
+                </a>
+              )}
             </div>
           </div>
         </div>
 
+        {/* Solution Section */}
         <div className="flex flex-col-reverse md:flex-row items-center md:gap-8 mt-2 md:mt-0">
           {detailsLocked && (
             <p className="text-red-400">
@@ -138,7 +133,7 @@ const UserDetails = ({ name, email, phone, website, company, id }: User) => {
           )}
 
           {!detailsRevealed && showInput && (
-            <div className="flex items-center gap-2 relative mb-2 md:mb-0 mt-8 md:mt-0">
+            <div className="flex items-center gap-2 relative mb-2 md:mb-0 mt-8 md:mt-0 min-w-48">
               {!detailsLocked && (
                 <p className="text-xs text-gray-400 absolute -top-4">
                   Solve to reveal
@@ -157,12 +152,6 @@ const UserDetails = ({ name, email, phone, website, company, id }: User) => {
                     className="bg-off-white border-[2px] border-gray-300 rounded-md w-20 p-1 text-black font-bold text-xl"
                   />
                 </>
-              )}
-
-              {!detailsLocked && attempts === 1 && (
-                <p className="text-xs text-red-400 absolute -bottom-4">
-                  Last chance!
-                </p>
               )}
             </div>
           )}
